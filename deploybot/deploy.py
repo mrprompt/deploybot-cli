e86a88f920015e8adcff8d7c404b4f7f6a7bed60
Thiago Paes <mrprompt@gmail.com>
2017-05-22 00:36:48 -0300

    small fixes

--- deploybot/deploy.py
+++ deploybot/deploy.py
@@ -4,7 +4,8 @@ from .client import Client
 
 class Deploy(Client):
     def list(self, repository="", environment=""):
-        url = "deployments?limit=20&repository_id=%s&environment_id=%s" % (repository, environment)
+        url = "deployments?limit=20&repository_id=%s&environment_id=%s" % \
+              (repository, environment)
         client = self.get_client(url)
         response = client.get()
 
